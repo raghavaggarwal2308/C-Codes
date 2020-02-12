e2b8d168d45a43a688f55542c58e080e5ff2895c
Raghav Aggarwal <55234496+raghavaggarwal2308@users.noreply.github.com>
2020-02-12 23:02:45 +0530

    Add files via upload
    rotations in sorted array

---
+++ Number of rotation.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    scanf("%d", &a[i]);
+    int sm=a[0];
+    for(int i=1;i<n;i++)
+        if(sm>a[i])
+        sm=a[i];
+    if(sm==a[0])
+    printf("-1");
+    else
+    {
+    int l=0, u=n-1,m;
+    while(l<u)
+    {
+        m = (l+u)/2;
+        /*if(n==2)
+        {
+            if(a[l]>a[u])
+            printf("%d", u);
+            else
+            printf("%d", l);
+            break;
+        }*/
+        if(a[u]>=a[l])
+            printf(-1);
+        else
+        {
+        if(a[m]<a[m+1] && a[m]<a[m-1])
+        {
+            printf("%d", m);
+            break;
+        }
+        if(a[u]>a[m])
+        {
+            u = m-1;
+        }
+        if(a[u]<a[m])
+        {
+            l = m+1;
+        }
+        }
+    }
+    }
+    return 0;
+}
